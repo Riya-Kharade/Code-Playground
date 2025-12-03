278f5fba1dc1825abd2fed1e7a1102f4a3dd1f96
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-03 12:41:42 +0530

    Add files via upload

---
+++ 4/flowchartCode.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class flowchartCode{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int ans=num1+num2;
+        if(ans>0){
+            System.out.println("Answer Is Positive"+ans);        
+        }
+        else{
+                        System.out.println("Answer Is Negative"+ans);        
+
+        }
+    }
+}
+
+/*
+This is simple code to check whether the sum of a given number  is positive or negative.
+its just a flowchart representation in java.
+*/

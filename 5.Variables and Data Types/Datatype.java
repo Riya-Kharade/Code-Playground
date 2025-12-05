dbfbee96bbaf72773dbccc1326abda5e03125145
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-06 00:01:07 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/Datatype.java
@@ -0,0 +1,29 @@
+public class Datatype {
+    public static void main(String args[]){
+        byte b=8;
+        System.out.println(b);
+
+        Short s=20;
+        System.out.println(s);
+
+        char ch='Z';
+        System.out.println(ch);
+
+        boolean by=true;
+        System.out.println(by);
+
+        int i=100;
+        System.out.println(i);      
+
+        float f=10.5f;
+        System.out.println(f);  
+
+        double d=99.99;
+        System.out.println(d);  
+
+        long l=123456789;
+        System.out.println(l);
+    }
+}
+
+//*This is simple Datatype Example in Java*/

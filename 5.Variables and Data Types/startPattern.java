2ca53fffd3d4d29b0f26864b2c3a12c98642fb13
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-05 23:39:38 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/startPattern.java
@@ -0,0 +1,16 @@
+public class startPattern {
+    public static void main (String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println("");
+        System.out.println("OR");
+        System.out.println("****\n***\n**\n*");
+
+
+
+    }
+}
+
+/*This is simple Star Pattern Without Using Any loop*/

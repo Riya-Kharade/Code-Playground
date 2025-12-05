54535e324f8a2a81ac6c75da242be46d816f5289
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-05 23:48:05 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(b);
+
+        String argss="Hello";
+        System.out.println(argss);
+
+        a=50;
+        System.out.println(a);
+
+    }
+}

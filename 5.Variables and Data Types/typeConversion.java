a38a85fb515e9e6201e75e05d79f189baef54844
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-07 00:41:18 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/typeConversion.java
@@ -0,0 +1,18 @@
+public class typeConversion {
+    public static void main(String args[]){
+        int a=10;
+        float b=a;
+
+        System.out.println("The value of b is:"+b);
+      
+        /*
+        float a=10.5f;
+        int b=a;
+        System.out.println("The value of b is:"+b);
+        
+
+        this gives error because we are trying to store a float value into an int variable directly.
+        */
+        
+    }
+}

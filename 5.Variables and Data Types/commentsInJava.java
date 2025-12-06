5754c56d0a2f53b43ece10fa58a56eefb5b54f70
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-06 23:39:50 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/commentsInJava.java
@@ -0,0 +1,13 @@
+public class commentsInJava {
+    public static void main(String args[]){
+        //This is single line comment in java
+
+    System.out.println("Hello, World!");
+
+    /*
+    This is Multi-line comment in java
+    */
+    }
+}
+
+//This is simple Comments Example in Java
---
+++ 5.Variables and Data Types/sumOfTwoNo.java
@@ -0,0 +1,11 @@
+public class sumOfTwoNo{
+    public static void main (String args[]){
+        int a=10;
+        int b=20;
+        int sum=a+b;
+
+        System.out.println("Sum of Two Number:"+sum);
+    }
+}
+
+//This is simple Sum of Two Number Example in Java

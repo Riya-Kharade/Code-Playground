d0524594f3375feb3dd57076d1a40be935b08846
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-07 20:22:07 +0530

    Add files via upload

---
+++ 5.Variables and Data Types/typeCasting.java
@@ -0,0 +1,13 @@
+public class typeCasting {
+    public static void main(String args[]){
+        float a=19.7f;
+        int b=(int)a;
+        System.out.println("The value of b is:"+b);
+
+
+        char ch='A';
+        int x=(int)ch;  
+        System.out.println("The ASCII value of A is:"+x);
+    }
+}
+/*This is Simple Type Casting Example */

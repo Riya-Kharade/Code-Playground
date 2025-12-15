b892e527f6a6941e98f0f39f3294fd5be0836185
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-12-15 23:13:44 +0530

    Add files via upload

---
+++ 9.Patterns part1/characterPattern.java
@@ -0,0 +1,20 @@
+public class characterPattern {
+        public static void main(String args[]){
+            char ch='A';
+		for(int line=1;line<=4;line++){
+			for(int star=1;star<=line;star++){
+				System.out.print(ch);
+                ch++;
+			}
+            System.out.println();
+		}
+	
+    }
+}
+/*
+output:
+A
+BC
+DEF
+GHIJ
+*/
